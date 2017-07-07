9613b8c64414c54d6aa51b555339b01b4ed21a0f
Jaime Rueda <rueda10@Jaimes-MacBook-Pro.local>
2017-07-07 11:19:52 -0500

    removed big images, changed img placeholder

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
 app.use("/", authRoutes);
 app.use("/", wishRoutes);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
   });
